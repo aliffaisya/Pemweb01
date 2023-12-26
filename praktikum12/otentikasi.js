651bcc70f8ad0339f6234fd73e78541b9292322f
aliffaisya <146052660+aliffaisya@users.noreply.github.com>
2023-12-26 14:50:38 +0700

    Add files via upload

---
+++ praktikum12/otentikasi.js
@@ -0,0 +1,16 @@
+const username = "fay"
+const password = "12345678"
+function auth(){
+    // tangkap nilai yang diinput user
+    let userInput = document.getElementById("username").value
+    let passwordInput = document.getElementById("password").value
+
+    let form = document.getElementById('form')
+
+    if(username == userInput && password == passwordInput){
+        alert("Login Berhasil")
+        form.submit()
+    } else{
+        alert("login Gagal!")
+    }
+}
